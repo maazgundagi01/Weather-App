01e8abe7fa798cd5f6941e6bebe72c90d16da0a3
Daniel Funk <dcfunkster@live.ca>
2022-03-12 15:43:04 -0600

    Creation of required components
    Created footer, header, saved-locations, search and weather-data components

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from 'header/Header.js';
+import WeatherData from 'weather-data/WeatherData.js';
+import Footer from 'footer/Footer.js';
 
 function App() {
   return (
     <>
-    
+
     </>
   );
 }
