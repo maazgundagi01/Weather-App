60a38020f9b963df3a64fc9e42888d710483ee45
maazgundagi01 <mgundagi.01@gmail.com>
2022-03-14 11:29:46 -0500

    placeholder table

--- weather-app/src/saved-locations/SavedLocations.js
+++ weather-app/src/saved-locations/SavedLocations.js
@@ -3,7 +3,28 @@ import React from 'react'
 function SavedLocations() {
 
     return (
-        <h1>locations</h1>
+        <div>
+            {/* //Table Just for styling purposes.
+            //Replace with...code...yep */}
+            <table>
+                <tr>
+                    <th>Locations</th>
+                    <th>Temperature</th>
+                </tr>
+                <tr>
+                    <td>Winnipeg, Canada</td>
+                    <td>-50'C</td>
+                </tr>
+                <tr>
+                    <td>Sahara Desert, Africa?</td>
+                    <td>50'C</td>
+                </tr>
+                <tr>
+                    <td>Tokyo, Japan</td>
+                    <td>0'C</td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
