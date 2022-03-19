1cc455aa08efd1f4622cdbbdf002884e409a2560
maazgundagi01 <mgundagi.01@gmail.com>
2022-03-19 16:21:44 -0500

    footer to class component

--- weather-app/src/footer/Footer.js
+++ weather-app/src/footer/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+// import React from 'react'
 import './footer.css'
-function Footer() {
-    return (
-        <footer>
-            <h4>Copyright 2022, The Conglomerate 2.0 && The Indian Conglomerate </h4>
-        </footer>
-    )
-}
+// function Footer() {
+//     return (
+//         <footer>
+//             <h4>Copyright 2022, The Conglomerate 2.0 && The Indian Conglomerate </h4>
+//         </footer>
+//     )
+// }
 
-export default Footer
+// export default Footer
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <h4>Copyright 2022, The Conglomerate 2.0 && The Indian Conglomerate </h4>
+        </footer>
+        )
+    }
+}
