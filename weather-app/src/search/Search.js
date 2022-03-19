73a232a511c631d0e3b0f0b14fdf05aed40cd7de
maazgundagi01 <mgundagi.01@gmail.com>
2022-03-19 16:11:04 -0500

    search function

--- weather-app/src/search/Search.js
+++ weather-app/src/search/Search.js
@@ -1,39 +1,47 @@
-import React from 'react'
-import Selector from './Selector'
+import { useState } from 'react'
+import data from './data.json'
 import './search.css'
 function Search() {
+    const [input, setInput] = useState("")
+    const displayer = (e) => {
+        e.preventDefault()
+        setInput(e.target.value);
+    };
+    if (input.length >= 0) {
+        data = data.filter((i) => {
+            return i.name.match(input.charAt(0).toUpperCase() + input.slice(1));
+        })
+    }
     return (
         <div className='content-parent'>
             <div className="content-child">
-            <Selector />
+                <div className='p-t-container'>
+
+                    <h2 className='page-title'>Search</h2>
+                    <h4 className='page-subtitle'>Search for a city or airport</h4>
+                    <div className='searcher'>
+                        <label htmlFor="s-l" ></label>
+                        <input type="text" className='s-in' placeholder='search location' onKeyDown={displayer} onLoad={displayer} value={input} />
+                    </div>
+                </div>
             <table className='data-table search-table'>
                 <tr className='data-tr head-tr'>
-                    <th>Locations</th>
-                    <th>Temperature</th>
+                        <th>City</th>
+                        <th>Country</th>
                 </tr>
-                <tr className='data-tr'>
-                    <td>Winnipeg, Canada</td>
-                    <td>-50'C</td>
-                </tr>
-                <tr className='data-tr'>
-                    <td>Sahara Desert, Africa?</td>
-                    <td>50'C</td>
-                </tr>
-                <tr className='data-tr'>
-                    <td>Tokyo, Japan</td>
-                    <td>0'C</td>
-                </tr>
-                <tr className='data-tr'>
-                    <td>South Pole, Antartica</td>
-                    <td>-100'C</td>
-                </tr>
-                <tr className='data-tr'>
-                    <td>Indonasian Volcano</td>
-                    <td>100'C</td>
-                </tr>
+                    {data.map((entry, lat) => {
+                        return (<>
+                            <tr key={lat} className='data-tr head-tr'>
+                                <td>{entry.name}</td>
+                                <td>{entry.country}</td>
+                                <button></button>
+                            </tr>
+                        </>);
+                    })}
                 </table>
             </div>
         </div>
+
     )
 }
 
