1fe7fb2c53ac2de0bf423e3ca69f89ef694eb61d
maazgundagi01 <mgundagi.01@gmail.com>
2022-03-20 18:36:55 -0500

    incomplete attempt to resolve search

--- weather-app/src/search/Search.js
+++ weather-app/src/search/Search.js
@@ -5,29 +5,45 @@ import './search.css'
 function Search() {
     let navigate = useNavigate();
     const [input, setInput] = useState("")
-
     const displayer = (e) => {
         e.preventDefault()
         setInput(e.target.value);
     };
-    if (input.length >= 0) {
+    const displayer2 = (e) => {
+        setInput(e.target.value);
+    };
+
+    if (input.length > 0) {
+        document.querySelector('table').style.display = "block"
+        // let trArray = document.querySelectorAll('tr')
+
+        // for (let index = 0; index < trArray.length; index++) {
+        //     const element = trArray[index];
+        //     element.style.display = 'block'
+        // }
         data = data.filter((i) => {
             return i.name.match(input.charAt(0).toUpperCase() + input.slice(1));
         })
     }
+    window.addEventListener('DOMContentLoaded', (event) => {
+        // let trArray = document.querySelectorAll('tr')
 
-
-
+        // for (let index = 0; index < trArray.length; index++) {
+        //     const element = trArray[index];
+        //     element.style.display = 'none'
+        // }
+        document.querySelector('table').style.display = "none"
+    });
     return (
         <div className='content-parent'>
             <div className="content-child">
                 <div className='p-t-container'>
-
                     <h2 className='page-title'>Search</h2>
                     <h4 className='page-subtitle'>Search for a city or airport</h4>
                     <div className='searcher'>
                         <label htmlFor="s-l" ></label>
-                        <input type="text" className='s-in' placeholder='search location' onChange={displayer} onLoad={displayer} value={input} />
+                        <input type="text" className='s-in' placeholder='search location' onChange={displayer} onKeyDown={displayer2} value={input} />
+
                     </div>
                 </div>
             <table className='data-table search-table'>
@@ -45,7 +61,6 @@ function Search() {
                                 <td>{entry.name}</td>
                                 <td>{entry.country}</td>
                                 <td><button onClick={() => { navigate(`/location/${entry.name}`) }} className='s-btn'> Go </button></td>
-
                             </tr>
                         </>);
                     })}
@@ -54,6 +69,6 @@ function Search() {
             </div>
         </div>
     )
-}
 
+}
 export default Search
